9908312d16097741c0d38ddea8945f06841c243b
NY case <15170282098@163.com>
2025-07-01 15:58:24 +0800

    调整练习文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef} from "react";
 import "./App.css";
-import  UserProfileWithData  from "./height";
 
 function App() {
-  return  <UserProfileWithData />;
+  const [count, setCount] = useState(0);
+  // 固定count为0，不再更新
+  useEffect(() => {
+    setCount(0);
+  }, []);
+  const handleClick = () => {
+    setTimeout(() => {
+      // ❶ 使用对象字面量强制立即求值（显示闭包中的真实值）
+      console.log("闭包中的count:", { count });
+      
+      // ❷ 通过闭包间接访问（可能被控制台延迟求值）
+      console.log("直接打印count:", count);
+       console.log("冻结值:", count);      
+    }, 2000);
+  };
 
-  // return (
-  //   <>
-
-  //   </>
-  // )
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 100)}>尝试更新</button>
+      <button onClick={handleClick}>Test Closure</button>
+    </div>
+  );
 }
 
 export default App;
